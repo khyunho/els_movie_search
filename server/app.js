f7296c5c2242085eaf994ea8bed7b0c8b0955c9a
khyunho <smacaron@naver.com>
2023-02-01 10:49:00 +0900

    fix: 권한 오류

--- server/app.js
+++ server/app.js
@@ -4,7 +4,13 @@ const port = 4000;
 
 const elasticsearch = require("elasticsearch");
 const client = new elasticsearch.Client({
-  hosts: ["http://localhost:9200"]
+    hosts: ["http://elastic:changeme@localhost:9200"]
+    // node: 'http://127.0.0.1:1358',
+    // auth: {
+    //     username: 'elastic',
+    //     password: 'changeme'
+    // }
+    // hosts: ["http://localhost:9200"]
   //프로토콜이 https이고 elasticsearch에 id, password가 있다면
   //hosts: ["https://es_id:es_pass@localhost:9200"]
 });
@@ -16,9 +22,10 @@ app.get('/', async (req, res) => {
         index: 'movie-data-nn',
         body: {
             query: {
-                match: {
-                    "movie.name": "해리"
-                }
+                match_all: {}
+                // match: {
+                //     "movie.name": "해리"
+                // }
             }
         }
     });
@@ -30,19 +37,3 @@ app.listen(port, () => {
     console.log(port);
 });
 
-
-
-// async function test() {
-//   try {
-//     const rs =  await client.search({
-//       index: 'movie-data-nn',
-//         query: {
-//           match_all : { }
-//       }
-//     });
-//     console.log(rs);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
-// test();
